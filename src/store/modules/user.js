3637887f5442f3e2e2178d513f8f435dd4792e79
Livfer <110990020@qq.com>
2020-04-21 09:59:10 +0800

    init

---
+++ src/store/modules/user.js
@@ -0,0 +1,100 @@
+import { login, getInfo } from "@/api/user";
+import { getToken, setToken, removeToken } from "@/utils/auth";
+import { resetRouter } from "@/router";
+
+const getDefaultState = () => {
+  return {
+    token: getToken(),
+    name: "",
+    avatar: ""
+  };
+};
+
+const state = getDefaultState();
+
+const mutations = {
+  RESET_STATE: state => {
+    Object.assign(state, getDefaultState());
+  },
+  SET_TOKEN: (state, token) => {
+    state.token = token;
+  },
+  SET_NAME: (state, name) => {
+    state.name = name;
+  },
+  SET_AVATAR: (state, avatar) => {
+    state.avatar = avatar;
+  }
+};
+
+const actions = {
+  // user login
+  login({ commit }, userInfo) {
+    const { username, password } = userInfo;
+    return new Promise((resolve, reject) => {
+      login({
+        login_name: username.trim(),
+        password: password,
+        remember_days: 7
+      })
+        .then(response => {
+          const { data } = response;
+          commit("SET_TOKEN", data.remember_token);
+          setToken(data.remember_token);
+          resolve();
+        })
+        .catch(error => {
+          reject(error);
+        });
+    });
+  },
+
+  // get user info
+  getInfo({ commit, state }) {
+    return new Promise((resolve, reject) => {
+      getInfo(state.token)
+        .then(response => {
+          const { data } = response;
+
+          if (!data) {
+            reject("Verification failed, please Login again.");
+          }
+
+          const { login_name, avatar } = data;
+
+          commit("SET_NAME", login_name);
+          commit("SET_AVATAR", avatar);
+          resolve(data);
+        })
+        .catch(error => {
+          reject(error);
+        });
+    });
+  },
+
+  // user logout
+  logout({ commit, state }) {
+    return new Promise((resolve, reject) => {
+      removeToken(); // must remove  token  first
+      resetRouter();
+      commit("RESET_STATE");
+      resolve();
+    });
+  },
+
+  // remove token
+  resetToken({ commit }) {
+    return new Promise(resolve => {
+      removeToken(); // must remove  token  first
+      commit("RESET_STATE");
+      resolve();
+    });
+  }
+};
+
+export default {
+  namespaced: true,
+  state,
+  mutations,
+  actions
+};
